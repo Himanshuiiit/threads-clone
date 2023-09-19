042ee680e9b3aa01153a168ccff74515cebaec79
Himanshu Verma <hims6482@gmail.com>
2023-09-19 14:26:28 +0530

    bug fix 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
