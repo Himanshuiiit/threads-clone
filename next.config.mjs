5d6e4eab25c3d9a93278d840046ae2bbf40b752d
Himanshu Verma <hims6482@gmail.com>
2023-09-19 14:22:47 +0530

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const config = {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
